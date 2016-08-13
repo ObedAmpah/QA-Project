6a2204e1976e394e51e68f933f3dbfed496665ba
Obed Ampah <oampah@gmail.com>
2016-08-13 17:42:41 -0400

    Got the palindrome flag text outputting and somewhat styled.

--- main.js
+++ main.js
@@ -5,36 +5,35 @@ $(document).ready(function() {
 
 	// Variable names should help you understand what is happening, not representative of the data
 	// Update: Variable names are not helping and you are lost
-	//TODO: FIX naming
+	// 3TODO: FIX naming
 
 
-		submitter.click(function() {
+	submitter.click(function() {
 
-			// Set a variable that contains the user input values
-			var inputVal = input.val();
-			
-			// split() method creates an array of the input values
-			var inputArrSplit = inputVal.split(' ');
+		// Set a variable that contains the user input values
+		var inputVal = input.val();
+		
+		// split() method creates an array of the input values
+		var inputArrSplit = inputVal.split(' ');
 
-			// The map() method loops over each item in an array and then we split the letters of each array item, reverse them, and rejoin them
-			var inputArrMap = inputArrSplit.map(function (item) {
-			    return item.split('').reverse().join('');
-			});
+		// The map() method loops over each item in an array and then we split the letters of each array item, reverse them, and rejoin them
+		var inputArrMap = inputArrSplit.map(function (item) {
+		    return item.split('').reverse().join('');
+		});
 
-			// 
-			for (var i = 0; i < inputArrMap.length; i++) {
+		for (var i = 0; i < inputArrMap.length; i++) {
 
-				$('#user_output').append("<span>" + inputArrMap[i] + "</span> ");
 
-				if (inputArrSplit[i] == inputArrMap[i]) {
-					console.log(inputArrSplit[i] + " is a palindrome");
-				}
+			if (inputArrSplit[i] == inputArrMap[i]) {
+
+				var palindromeFlag = $('#user_output').append('<span>' + inputArrMap[i] + '</span>');
+
+				palindromeFlag.append('<figcaption> That is a palindrome. Cool! </figcaption>');
 
 			}
 
-		});
+		}
 
-
-
+	});
 
 });
